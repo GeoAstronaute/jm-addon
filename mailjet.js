892867c6993cf0dccb60b09e46d09454d144b42c
GeoAstronaute <jeanmarc.royy@gmail.com>
2018-02-16 04:03:16 +0100

    Update mailjet bundle

--- mailjet.js
+++ mailjet.js
@@ -1,9 +1,12 @@
 System.register(['@angular/core', 'plugins-core'], function (exports, module) {
 'use strict';
-var Component, Extension;
+var Component, Input, Output, EventEmitter, Extension;
 return {
 setters: [function (module) {
 Component = module.Component;
+Input = module.Input;
+Output = module.Output;
+EventEmitter = module.EventEmitter;
 }, function (module) {
 Extension = module.Extension;
 }],
@@ -46,10 +49,61 @@ function __metadata(metadataKey, metadataValue) {
 
 var MailjetViewComponent = (exports('MailjetViewComponent', function () {
     function MailjetViewComponent() {
+        this.emails = [];
+        this.openSetup = new EventEmitter();
     }
     MailjetViewComponent.prototype.ngOnInit = function () {
         console.log("Addon view INIT");
+        this.init();
+        this.loadTemplates();
     };
+    MailjetViewComponent.prototype.cancel = function () {
+        console.log("Click cancel");
+    };
+    MailjetViewComponent.prototype.send = function (ev) {
+    };
+    MailjetViewComponent.prototype.init = function () {
+        var _this = this;
+        if (this.app && this.app.entities && this.app.entities.get("mailjet")) {
+            this.app.entities.get("mailjet").getQuery("latest").run().then(function (emails) {
+                _this.emails = emails.data.slice();
+                _this.nbEmails = emails.count;
+            }).catch(function (e) {
+                console.log("error", e, e.stack);
+            });
+        }
+        else {
+            setTimeout(function () {
+                _this.init();
+            }, 100);
+        }
+    };
+    MailjetViewComponent.prototype.loadTemplates = function () {
+        var _this = this;
+        this.app.entities.get("mailjet").getQuery("getTemplates").run().then(function (files) {
+            _this.templates = files;
+        });
+    };
+    MailjetViewComponent.prototype.selectTemplate = function (template) {
+        this.templateSelected = Object.assign({}, template);
+        this.lastUpdatedCode = template.code;
+        this.loadPreview(template.code);
+    };
+    MailjetViewComponent.prototype.lastCodeEv = function (ev) {
+        this.lastUpdatedCode = ev;
+        return this.loadPreview(ev);
+    };
+    MailjetViewComponent.prototype.loadPreview = function (code) {
+        this.code = code;
+    };
+    __decorate([
+        Input(),
+        __metadata("design:type", Object)
+    ], MailjetViewComponent.prototype, "app", void 0);
+    __decorate([
+        Output(),
+        __metadata("design:type", Object)
+    ], MailjetViewComponent.prototype, "openSetup", void 0);
     MailjetViewComponent = __decorate([
         Extension("materia-mailjet-view", []),
         Component({
